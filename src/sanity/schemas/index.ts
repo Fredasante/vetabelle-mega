a288ec94730a1feb3ba649974422a5d067ae9abf
Alfred Apenteng <alfredapenteng6996@gmail.com>
2026-04-22 22:28:02 +0000

    feat(sanity): register masterclass schemas in studio

--- src/sanity/schemas/index.ts
+++ src/sanity/schemas/index.ts
@@ -1,4 +1,6 @@
 import order from "./order";
 import product from "./product";
+import masterclass from "./masterclass";
+import masterclassRegistration from "./masterclassRegistration";
 
-export const schemas = [product, order];
+export const schemas = [product, order, masterclass, masterclassRegistration];
